76ce997077681f06953fd1307a847bb3ac9cf9cf
AdrianaTR80 <31289704+AdrianaTR80@users.noreply.github.com>
2017-10-22 22:29:01 -0500

    Add files via upload

---
+++ Tarea.cpp
@@ -0,0 +1,33 @@
+// Tarea leer diagrama de flujo
+// Adriana Tapia Ramírez
+// Fisica Computacional
+# include  < iostream >
+
+usando  namespace  std ;
+
+int  main ()
+{
+    int s = 12 , p, i;
+    int prime = 1 ;
+    int f = 1 ;
+    cout << " Teclea un número entero positivo: " << endl;
+    cin >> p;
+    hacer {
+        hacer {
+            if (p% 2 == 0 ) p = p + 1 ;
+            para (i = 2 ; i <= p / 2 ; ++ i)
+                if (p% i == 0 )
+                {primo = 0 ;
+                    p = p + 2 ;
+                }
+                else primo = 1 ;
+        } while (prime = 0 );
+        if (p <s) {s = sp;}
+        else {s = s- 1 ;}
+        if (s == 0 ) { break ;}
+        else {p = p + 2 ;}
+    } while (f = 1 );
+    
+    cout << " P = " << p << endl;
+    return  0 ;
+}
